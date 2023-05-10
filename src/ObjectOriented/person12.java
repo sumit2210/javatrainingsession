f103d90426a085418f76d921118da6eea046409c
Sumit <paliwalrtm9033@gmail.com>
2023-05-10 07:01:59 +0530

    Initial commit

---
+++ src/ObjectOriented/person12.java
@@ -0,0 +1,21 @@
+/*package ObjectOriented;
+
+import java.util.Scanner;
+
+public class person {
+    private int input1 = 0;
+    private String input2 = "";
+    private Scanner scanner = new Scanner(System.in);
+
+    public Adder(int i1, String i2){
+        this.input1 = i1;
+        this.input2 = i2;
+    }
+    public void Adder(int input1){
+        this.input1 = input1;
+        this.input2 = String.valueOf(input1);
+    }
+
+}
+
+ */
---
+++ src/ObjectOriented/summary.java
@@ -0,0 +1,2 @@
+package ObjectOriented;public class summary {
+}
